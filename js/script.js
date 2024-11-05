6a84ca932148555cbdf8fec4aaeacbad1c7e5804
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-05 15:08:23 +0100

    add function and axios get

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
+//FUCTIONS
+function createEmail(){
+    axios
+    .get("https://flynn.boolean.careers/exercises/api/random/mail") //vado a mandare una request
+    .then((result=>{
+        
+    }))
+    .catch((err)=>console.log(err))
+
+}
+
+//MAIN CODE
 button.addEventListener("click", (event)=>{
     event.preventDefault();
-
+    createEmail();
 })
