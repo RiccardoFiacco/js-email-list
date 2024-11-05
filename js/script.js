f48c67cd293ac8073433b18417b4295e78c6fdf8
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-05 17:24:54 +0100

    add a possible solution

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
 //FUCTIONS
-function createEmail(){
-    let email = '';
-    let emails = [];
+function createEmail(callback) {
+  let email = "";
+  let emails = [];
 
-    for(let i=0; i < 10; i++){
-        axios
-        .get("https://flynn.boolean.careers/exercises/api/random/mail") //vado a mandare una request
-        .then((result=>{
-            email = result.data.response;
-            emails.push(email);
-            console.log(emails);        
-        }))
-        .catch((err)=>console.log(err));
-    }
-    return emails;
+  for (let i = 0; i < 10; i++) {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail") //vado a mandare una request
+      .then((result) => { // Questo codice qui viene eseguito QUANDO la chiamata ha ricevuto la risposta e il thread principale è libero
+        email = result.data.response;
+        callback(email);
+      })
+      .catch((err) => console.log(err));
+  }
+  return emails;
 }
 
-
 //MAIN CODE
-button.addEventListener("click", (event)=>{
-    event.preventDefault();
-    createEmail();
+button.addEventListener("click", (event) => {
+  event.preventDefault();
 
-})
+  app = createEmail((email) => {
+    const el = document.createElement('p');
+    el.innerHTML += email;
+    printOnW.appendChild(el);
+  });
+  printOnW.innerText = " ";
+});
