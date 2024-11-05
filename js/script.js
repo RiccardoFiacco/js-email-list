4e87102e87229460e7621c52b3ed2b32e1e628f8
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-05 15:44:04 +0100

    add creation of array with ten emails

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 //FUCTIONS
 function createEmail(){
-    axios
-    .get("https://flynn.boolean.careers/exercises/api/random/mail") //vado a mandare una request
-    .then((result=>{
-        
-    }))
-    .catch((err)=>console.log(err))
+    let email = '';
+    let emails = [];
 
+    for(let i=0; i < 10; i++){
+        axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail") //vado a mandare una request
+        .then((result=>{
+            email = result.data.response;
+            emails.push(email);
+            console.log(emails);        
+        }))
+        .catch((err)=>console.log(err));
+    }
+    return emails;
 }
+
 
 //MAIN CODE
 button.addEventListener("click", (event)=>{
     event.preventDefault();
     createEmail();
+
 })
